2b5d32cb450ca3b81e2975ab260cc24b7fb670ac
Nagasatish Vadlamuri <91796133+awsop@users.noreply.github.com>
2021-12-28 21:20:01 +0530

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -3,21 +3,20 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 // 1) when seeding the database you'll have to uncomment this!
-// import { seedDatabase } from '../seed';
+ import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: "AIzaSyCvIOzitxhJCtc-K_OF0L_QkEse9owObL0",
+  authDomain: "netflix-k8s.firebaseapp.com",
+  projectId: "netflix-k8s",
+  storageBucket: "netflix-k8s.appspot.com",
+  messagingSenderId: "561539661219",
+  appId: "1:561539661219:web:6f0b991f20abbe8d782e16"
 };
 
 const firebase = Firebase.initializeApp(config);
 // 2) when seeding the database you'll have to uncomment this!
-// seedDatabase(firebase);
+ seedDatabase(firebase);
 // 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
 
 export { firebase };
